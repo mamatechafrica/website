54cad1bddff876183f4fd40c8938882625bc9169
nashthecoder <naijeria@gmail.com>
2024-11-13 02:03:19 +0300

    added mailtrap domain host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,8 @@ config.action_mailer.smtp_settings = {
   authentication: "login",
   enable_starttls_auto: true
 }
+
+config.action_mailer.default_url_options = { host: 'sandbox.smtp.mailtrap.io' }
   # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
