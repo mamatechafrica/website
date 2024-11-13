fb023adb34ae60f508604b995105dac3a68567a3
nashthecoder <naijeria@gmail.com>
2024-11-13 21:39:11 +0300

    modified:   config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,20 +39,27 @@ Rails.application.configure do
 
 
 # place this code in config/environments/production.rb:
-# config.action_mailer.delivery_method = :mailtrap
-# config.action_mailer.mailtrap_settings = {
-#   api_key: ENV.fetch("MAILTRAP_API_KEY")
-# }
+config.action_mailer.delivery_method = :mailtrap
+config.action_mailer.mailtrap_settings = {
+  api_key: ENV.fetch("MAILTRAP_API_KEY")
+}
 
+
+config.action_mailer.delivery_method = :mailtrap
+config.action_mailer.mailtrap_settings = {
+  api_key: '********0d7b',
+  sandbox: true,
+  inbox_id: 3263693,
+}
 # Looking to send emails in production? Check out our Email API/SMTP product!
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  user_name: 'd47046b342d07a',
-  password: '28ed2988abaa52',
-  address: 'sandbox.smtp.mailtrap.io',
-  host: 'sandbox.smtp.mailtrap.io',
-  port: '2525',
-  authentication: :login
+# config.action_mailer.delivery_method = :smtp
+# config.action_mailer.smtp_settings = {
+#   user_name: 'd47046b342d07a',
+#   password: '28ed2988abaa52',
+#   address: 'sandbox.smtp.mailtrap.io',
+#   host: 'sandbox.smtp.mailtrap.io',
+#   port: '2525',
+#   authentication: :login
 }
   # then set the MAILTRAP_API_KEY environment variable
   # using your hosting solution.
